c222bbc00b9409a70921328ed8e359fab2a2a73e
Zbarskyi <zvn86@ukr.net>
2025-09-16 19:06:57 +0300

    JS_HW_4

---
+++ JS_HW_4.js
@@ -0,0 +1,28 @@
+function checkProbabilityTheory(count) {
+    let evenCount = 0;
+    let oddCount = 0;
+    for (let i = 0; i < count; i++) {
+        let number = Math.floor(Math.random() * (1000 - 100 + 1)) + 100;
+        if (number % 2 === 0) {
+            evenCount++;
+        } else {
+            oddCount++;
+        }
+    }
+    console.log( count);
+    console.log( evenCount);
+    console.log( oddCount);
+    let percentEven = ((evenCount / count) * 100).toFixed(2);
+    let percentOdd = ((oddCount / count) * 100).toFixed(2);
+    console.log( percentEven + "%");
+    console.log(percentOdd + "%");
+}
+
+
+
+
+
+
+
+
+
