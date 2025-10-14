b1197c51c0a5a4beb3f6e67c3dcb64c859beb950
Zbarskyi <zvn86@ukr.net>
2025-10-14 19:07:25 +0300

    JS_HW_7.1.js

---
+++ JS_HW_7.1.js
@@ -0,0 +1,26 @@
+var arr = [
+    {
+        userName: "Test",
+        lastName: "Test",
+        email: "test.test@gmail.com"
+    },
+    {
+        userName: "Dmitro",
+        lastName: "Porohov",
+        email: "dmitro.porohov@yahoo.com"
+    },
+    {
+        userName: "Andrii",
+        lastName: "",
+        email: "andrii@mail.ru" 
+    },
+];
+var emailPattern = /^[A-Za-z0-9]+(\.[A-Za-z0-9]+)?@(gmail\.com|yahoo\.com)$/
+var trustedEmails = [];
+for (var i = 0; i < arr.length; i++) {
+    var email = arr[i].email; 
+    if (emailPattern.test(email)) { 
+        trustedEmails.push(email); 
+    }
+}
+console.log(trustedEmails);
